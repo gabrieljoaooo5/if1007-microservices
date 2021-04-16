5597820356e3fe575e30f98b9457f1b1e13ac021
biel302 <gabriel.longuinhos@hotmail.com>
2021-04-15 21:51:07 -0300

    routes backend e ajuster FE

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const UserController = require('./controllers/UserController');
+const TrelloController = require('./controllers/ImportToTrello');
 
 const routes = express.Router();
 
 routes.post('/users', UserController.store);
 routes.get('/users', UserController.show);
 routes.put('/users', UserController.update);
+
+routes.put('/importTrello', TrelloController.import);
 
 module.exports = routes;
