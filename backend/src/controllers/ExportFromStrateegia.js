41851216c1f7b004158c6a3145c207b30fb8f1a8
jgsa <jgsa@dmz.org.br>
2021-04-18 17:37:48 -0300

    Export kits from strateegia

--- backend/src/controllers/ExportFromStrateegia.js
+++ backend/src/controllers/ExportFromStrateegia.js
@@ -1,4 +1,10 @@
 const User = require('../models/User');
+const bodyParser = require('body-parser');
+let infoProject = [];
+let infoKit = [];
+let infoAllProjects = [];
+let returnProjects = [];
+let infoMission = [];
 
 module.exports = {
 
@@ -7,14 +13,19 @@ module.exports = {
             const { email } = req.body;
             const fetch = require('node-fetch');
             const user = await User.findOne({ email: email });
-            const token = user.token;
+            const token = "";
+            console.log(token);
+            let projects;
+            let missions;
+            let contents;
 
             //Vai buscar todos os projetos do usuário
-            let projects = fetch('https://api.strateegia.digital/projects/v1/project', {
+            let projectsResponse = await fetch('https://api.strateegia.digital/projects/v1/project', {
                 method: 'GET',
+                Authorization: `Bearer ${token}`,
                 headers: {
-                    'Accept': 'application/json',
-                    'Auth': `Bearer ${token}`
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
                 }
                 })
                 .then(response => {
@@ -23,18 +34,27 @@ module.exports = {
                     );
                     return response.text();
                 })
-                .then(text => console.log(text))
+                .then(text => projects = JSON.parse(text))
                 .catch(err => console.error(err));
-
             if(projects) {
-                projects = projects.projects
-                for (var project in projects) {
-                    let projectId = project.id
-                    let missions = fetch(`https://api.strateegia.digital/projects/v1/project/${projectId}`, {
+                projects = projects.map(function (item) { return item.projects});
+                let lengthProjects = projects.length;
+                for (var i = 0; i < lengthProjects; i++){
+                    for (var j=0; j < projects[0].length; j++){
+                        projects.push(projects[0][j]);
+                    }
+                    projects.shift();
+                }
+                lengthProjects = projects.length;
+                for (var i = 0; i < lengthProjects; i++) {
+                    let project = projects[i];
+                    let projectId = project.id;
+                    console.log("projectid");
+                    let missionsResponse = await fetch(`https://api.strateegia.digital/projects/v1/project/${projectId}`, {
                         method: 'GET',
                         headers: {
-                            'Accept': 'application/json',
-                            'Auth': `Bearer ${token}`
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${token}`
                         }
                         })
                         .then(response => {
@@ -43,18 +63,20 @@ module.exports = {
                             );
                             return response.text();
                         })
-                        .then(text => console.log(text))
+                        .then(text => missions = JSON.parse(text))
                         .catch(err => console.error(err));
-
                     if(missions) {
-                        missions = missions.missions
-                        for (var mission in missions) {
+                        missions = missions.missions;
+                        let lengthMissions = missions.length;
+                        for (var j = 0; j < lengthMissions; j++) {
+                            let mission = missions[j];
                             let missionId = mission.id
-                            let contents = fetch(`https://api.strateegia.digital/projects/v1/map/${missionId}`, {
+                            console.log("missionId");
+                            let contentsResponse = await fetch(`https://api.strateegia.digital/projects/v1/map/${missionId}/content`, {
                                 method: 'GET',
                                 headers: {
-                                    'Accept': 'application/json',
-                                    'Auth': `Bearer ${token}`
+                                    'Content-Type': 'application/json',
+                                    'Authorization': `Bearer ${token}`
                                 }
                                 })
                                 .then(response => {
@@ -63,20 +85,38 @@ module.exports = {
                                     );
                                     return response.text();
                                 })
-                                .then(text => console.log(text))
+                                .then(text => contents = JSON.parse(text))
                                 .catch(err => console.error(err));
-
-                            return res.json(contents.content);   
+                            // console.log(`contents`);
+                            // console.log(contents);
+                            // infoKit.push({
+                            //     key:   mission.title,
+                            //     value: contents
+                            // });
+                            contents = contents.content
+                            console.log(contents)
+                            contents = contents.map(function (item) { return item.kit});
+                            infoMission.push({
+                                key: mission.title,
+                                value: contents
+                            });
+                            returnProjects.push(contents);   
                         }
+                        infoProject.push({
+                            key: project.title,
+                            value: infoMission
+                        })
+                        infoMission = [];
                     }
                 }
             }
         } catch (error){
-            response.json({
+            res.json({
                 error: true,
                 message: error.message
             });
             
         }
+        return res.json(infoProject);
     }   
 };
