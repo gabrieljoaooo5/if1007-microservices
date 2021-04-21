896c2b3a971783005532ba91112890336b1c5aec
jgsa <jgsa@dmz.org.br>
2021-04-21 16:38:50 -0300

    Get token from api strateegia

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,17 +1,96 @@
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
+const fetch = require('node-fetch');
+
+async function getToken(email, password) {
+    // var request = require('request');
+
+
+    // var options = {
+    //     url: 'https://api.strateegia.digital/users/v1/auth/signin',
+    //     method: 'GET',
+    //     headers: { 'Authorization': 'Basic jgsa@cin.ufpe.br:' },
+    //     formData: {
+    //         'username':'jgsa@cin.ufpe.br',
+    //         'password':''
+    //     },
+
+    // }
+
+    // request.get(options, function(err,res,body) {
+    //     if(err){
+    //         console.log(err);
+    //         return;
+    //     }
+    //     console.log('status code :', res.statusCode);
+    // });
+//     var request = require('request');
+//         url = "https://api.strateegia.digital/users/v1/auth/signin",
+//         form
+
+// request(
+//     {
+//         url : "https://api.strateegia.digital/users/v1/auth/signin",
+//         formData: {
+//             'username': 'jgsa@cin.ufpe.br',
+//             'password': ''
+//         }
+//         // headers : {
+//         //     "Authorization" : "Basic "
+//         // }
+
+//     },
+//     function(error, response, body){
+//         if(error) {
+//             console.log(error);
+//         } else {
+//             console.log(response.statusCode, body);
+//             json = JSON.parse(response.body);
+//             console.log(response.statusCode, body);
+//     }
+//     });
+
+    let token = '';
+    let projectsResponse = await fetch('https://api.strateegia.digital/users/v1/auth/signin', {
+                method: 'GET',
+                formData: {
+                            'username':'',
+                            'password':''
+                },
+                user: '',
+                password: '',
+                Headers: {
+                    'Authorization': 'Basic ' + Buffer.from('' + ":" + '').toString('base64')
+                }
+                })
+                .then(response => {
+                    console.log(
+                    `Response: ${response.status} ${response.statusText}`
+                    );
+                    if (response.status == 403) {
+                        console.log('erro aq');
+                        return '403';
+                    }
+                    return JSON.parse(response.text());
+                })
+                .then(response => token = response);
+    return '403';
+} 
 
 module.exports = {
     async store(req, res) {
 
         const { email, password } = req.body;
-
+        let encryptedPassword = CryptoJS.SHA256(password);
+        encryptedPassword = encryptedPassword.toString(CryptoJS.enc.Base64);
         let user = await User.findOne({ email });
-
-        if(!user){
+        let strateegia_token = await getToken(email, password);
+        console.log(strateegia_token);
+        if(!user && strateegia_token != '403'){
             const tokenTrello = '#';
-            const token = '#';
+            const token = strateegia_token.access_token;
             const consumerKey = '#';
-            user = await User.create({ email, password, token, consumerKey, tokenTrello });
+            user = await User.create({ email, encryptedPassword, token, consumerKey, tokenTrello });
         }
 
         return res.json(user);
